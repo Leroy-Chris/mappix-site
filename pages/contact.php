db9d057677d15c091c9d11a3ebdf4bdd56774f8e
Chris Leroy <email_github@example.com>
2026-03-02 16:58:41 +0100

    TP FINI DANS 15 MINUTES

--- pages/contact.php
+++ pages/contact.php
@@ -13,4 +13,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo "<p>Merci " . htmlspecialchars($_POST["nom"]);
 }
 ?>
-iugrjbiffesuifbseuf                                                ioutiyufuiyt
+iugrjbiffesuifbseuf           sdhibfsygfg                                     ioutiyufuiyt
